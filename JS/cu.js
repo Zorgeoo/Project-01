12fe55827df72b47f8e067e818545ddfce20632f
LPD243578 LPD243578 <LPD243578@G301-CC0202.local>
2024-05-28 12:44:51 +0800

    last

--- JS/cu.js
+++ JS/cu.js
@@ -68,18 +68,19 @@
 //             return console.log('not a prime number')
 //     }
 // }
-function isPrime(number) {
-    // Check if the number is less than 2
-    if (number < 2) {
-        return false;
-    }
-    // Check for divisibility by numbers from 2 to the square root of the number
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-        if (number % i === 0) {
-            return false; // If divisible, the number is not prime
-        }
-    }
-    return true; // If not divisible by any number, the number is prime
-}
-console.log(isPrime(5))
-console.log(Math.sqrt(5))
+// function isPrime(number) {
+//     // Check if the number is less than 2
+//     if (number < 2) {
+//         return false;
+//     }
+//     // Check for divisibility by numbers from 2 to the square root of the number
+//     for (let i = 2; i <= Math.sqrt(number); i++) {
+//         if (number % i === 0) {
+//             return false; // If divisible, the number is not prime
+//         }
+//     }
+//     return true; // If not divisible by any number, the number is prime
+// }
+// console.log(isPrime(5))
+// console.log(Math.sqrt(5))
+
--- JS/pp.js
+++ JS/pp.js
@@ -14,7 +14,7 @@
 // const arr = count(10)
 // console.log(arr)
 
-const a = [5, 10, 7, 8, 49];
+// const a = [5, 10, 7, 8, 49];
 
 // function printArr(arr) {
 //     for (let i = 0; i < arr.length; i = i + 1) {
@@ -33,14 +33,66 @@ const a = [5, 10, 7, 8, 49];
 // }
 // console.log(even(a))
 
-const b = [65, 62, 46, 875, 503266, 62432,4564564894]
+// const b = [65, 62, 46, 875, 503266, 62432, 4564564894]
 
-function max(arr) {
-    let count=arr[0]
-    for (let i = 1; i < arr.length; i=i+1){
-        if (arr[i]>count){
-            count=arr[i]
+// function max(arr) {
+//     let count = arr[0]
+//     for (let i = 1; i < arr.length; i = i + 1) {
+//         if (arr[i] > count) {
+//             count = arr[i]
+//         }
+//     } return count
+// }
+// console.log(max(b))
+
+// const band = [20, 25, 45, 63]
+
+// const max = (arr) => {
+//     let count = arr[0];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] > count) {
+//             count = arr[i]
+//         }
+//     }
+//     return count
+// }
+
+
+// const result = max(band)
+// console.log(result)
+
+// let person = {
+//     firstName: "Enkhzorig",
+//     lastName: "Enkhbaatar",
+//     age: 25,
+//     gender: "male",
+//     eyeColor: "brown",
+//     hairColor: "brown",
+//     nationality: "Mongolia",
+//     married: false,
+//     body: {
+//         weight: '70kg',
+//         height: "182cm"
+//     },
+//     hobby: ["football", "basketball", "esport", "cooking"],
+//     marriage: ["gerlesen", "gerleegui"]
+// }
+
+
+// console.log(`Hello. My name is ${person.firstName, person.lastName}. I am ${person.age} years old. My hair color is ${person.eyeColor}. I am from ${person.nationality}. ${person.married}. I weigh ${person.body.weight}. My height is ${person.body.height}. My hobbies are ${person.hobby[0]}. i am ${person.marriage(single)}`)
+
+const a = [1, 1, 1, 3, 3, 3, 4, 4, 4, 7, 7, 7, 1]
+
+const number = (arr) => {
+    let count = {}
+    for (let i = 0; i < arr.length; i++) {
+        const num = arr[i];
+        if (count[num] !== undefined) {
+            count[num]++;
+        } else {
+            count[num] = 1
         }
-    }  return count
+    } return count
 }
-console.log(max(b))
+console.log(number(a))
