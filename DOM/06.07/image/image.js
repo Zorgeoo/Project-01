5584a9ab4c0228150e9234c484c4f81cee7c7fea
HOME <zjergsen@gmail.com>
2024-06-09 21:47:17 +0800

    asdas

--- DOM/06.07/image/image.js
+++ DOM/06.07/image/image.js
@@ -1,6 +1,18 @@
-const next = document.querySelector('#next')
-const images = document.querySelectorAll('img')
-
-next.addEventListener("click", () => {
-    images.style.border = '2px solid red'
+const slide = document.querySelector('.box')
+const nextButton=document.querySelector("#next")
+const prevButton=document.querySelector("#previous")
+// slide.addEventListener("wheel",(evt)=>{
+//     evt.preventDefault()
+//     slide.scrollLeft+=evt.deltaY;
+// })
+nextButton.addEventListener("click",()=>{
+    slide.style.scrollBehavior="smooth";
+    slide.scrollLeft+=300;
 })
+
+prevButton.addEventListener("click",()=>{
+    slide.scrollLeft-=300;
+    slide.style.scrollBehavior="smooth";
+})
+
+
