b3cccc9df2a87418c74848507caea24aee53c709
LPD243578 LPD243578 <LPD243578@G301-CC0202.local>
2024-06-17 13:57:03 +0800

    dasas

--- DOM/06.07/06.13/reactLogic.js
+++ DOM/06.07/06.13/reactLogic.js
@@ -5,12 +5,19 @@ const esc = document.querySelector(".esc")
 
 const form = document.querySelector("form")
 const cards = document.querySelector(".cards")
+const inProgress = document.querySelector(".inProgress")
+const blocked = document.querySelector(".blocked")
+const done = document.querySelector(".done")
+const checkBox = document.querySelector(".myCheckBox")
+
 const card = document.querySelector(".card")
 
 
 const titleInput = document.querySelector(".title")
 const textInput = document.querySelector(".textArea")
-
+const selectorInput = document.getElementById("selector")
+const statusSelectorInput = document.querySelector("#statusSelector")
+let index = 0
 
 openModal.addEventListener("click", (event) => {
     event.preventDefault()
@@ -21,12 +28,14 @@ esc.addEventListener("click", (event) => {
     overlay.classList.remove("active")
 })
 
-const myFunction = (title, description) => {
+const myFunction = (title, description, id, selector, status) => {
     return `<div class="card">
-    <button class="small">X</button>
+    <input type="checkbox" id="checkbox-${id}" onchange="switchItem(${id})">
+    <button onclick="deleteItem(${id})" class="small" >X</button>
+    <div>Priority:${selector}</div>
+    <div>Status:${status}</div>
     <h1>${title}</h1>
-    <p>${description}</p>
-    </div>`
+    <p>${description}</p>`
 }
 
 
@@ -37,36 +46,76 @@ let data = []
 
 const setData = (arr) => {
     data = arr;
-
+    sortData()
     render();
 }
 
+// Sort 
+
+const sortData = () => {
+    data.sort((a, b) => {
+        const priorityOrder = { "high": 1, "medium": 2, "low": 3 }
+        return priorityOrder[a.selector] - priorityOrder[b.selector];
+    })
+}
 //Render
 
 const render = () => {
     cards.innerHTML = ""
+    blocked.innerHTML = ""
+    inProgress.innerHTML = ""
+    done.innerHTML = ""
 
     data.forEach((item) => {
-        cards.innerHTML += myFunction(item.title, item.description);
-    })
+        if (item.status === "To Do") {
+            cards.innerHTML += myFunction(item.title, item.description, item.id, item.selector, item.status);
+        } else if (item.status === "Blocked") {
+            blocked.innerHTML += myFunction(item.title, item.description, item.id, item.selector, item.status);
+        } else if (item.status === "In Progress") {
+            inProgress.innerHTML += myFunction(item.title, item.description, item.id, item.selector, item.status);
+        } else if (item.status === "Done") {
+            done.innerHTML += myFunction(item.title, item.description, item.id, item.selector, item.status);
+        }
+    }
+    )
 }
+
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
     const title = titleInput.value;
     const description = textInput.value;
+    const selector = selectorInput.value;
+    const status = statusSelectorInput.value;
 
     const newData = [
         ...data,
         {
+            "id": index,
             title,
             description,
+            selector,
+            status,
         }
-    ]
+    ];
+    index++;
     setData(newData);
     overlay.classList.remove("active")
 });
 
-render();
-console.log(data)
+const deleteItem = (id) => {
+    const newData = [...data].filter((item) => item.id !== id);
+    setData(newData);
+};
+const switchItem = (id) => {
+    const newData = data.map((item) => {
+        if (item.id === id) {
+            return { ...item, status: "Done" };
+
+        } return item
+    });
+    setData(newData)
+}
+
+// render();
